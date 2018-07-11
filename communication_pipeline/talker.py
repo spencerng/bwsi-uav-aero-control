5afe5054bfa323a91270eb2c891acf05a415d5c4
Student@BWSI <you@example.com>
2018-07-11 16:44:24 -0400

    Turned aero_control into a ROS package

---
+++ communication_pipeline/talker.py
@@ -0,0 +1,17 @@
+
+
+'''TODO-START: FILL IN CODE HERE
+* create a string message that contains "Hello World" and the iteration number i
+'''
+raise Exception("Hello World")
+'''TODO-END '''
+
+
+
+'''TODO-START: FILL IN CODE HERE
+* create a string message that contains "Hello World" and the iteration number i
+'''
+raise Exception("Hello World")
+'''TODO-END '''
+
+
