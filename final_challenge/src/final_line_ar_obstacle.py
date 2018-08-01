21cc0eb4e79b521f28dc42a0278db8f2d63501ad
Spencer Ng <sng1488@gmail.com>
2018-08-01 15:27:30 -0400

    Begin final challenge

---
+++ final_challenge/src/final_line_ar_obstacle.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+import rospy
+import threading
+import numpy as np
+import datetime
+from geometry_msgs.msg import TwistStamped, Twist, PoseStamped, Quaternion, Point, Vector3
+from sensor_msgs.msg import Image
+from aero_control.msg import Line
+from std_msgs.msg import Float32
+import cv2
+import mavros
+from mavros_msgs.msg import State
+from cv_bridge import CvBridge, CvBridgeError
+from copy import deepcopy
+from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
+
+
+NO_ROBOT =True # set to True to test on laptop
+MAX_ANG_SPEED = np.pi/2  #[rad/s]
+MAX_LIN_SPEED = .5 # [m/s]
+DISTANCES = {9:(0.0,0.0,-1.0),12:(0.0,0.0,1.0),24:(0.0,0.0,1.0), 16:(0.0,0.0,1.0)} 
+AR_FWD_THRESH = 1.0
+AR_FWD_TOL = 0.25
+Z_LIN_SPEED = 1.0
+
+class FinalChallengeController:
+	def __init__(self, rate=10):
+		assert rate > 2 # make sure rate is high enough, if new setpoint recieved within .5 seconds, robot will switch back to POSCTL
+		self.rate_hz = rate
+		self.pub_pid_vel = rospy.Subscriber("/line_detection/cmd_vel", Vector3, self.line_vel_cb)
+		self.vx_line = 0
+		self.vy_line = 0
+		self.yaw = 0
+		self.state = 1
+		self.ar_pose_sub = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.ar_pose_cb, queue_size = 1)
+		self.pose_sub = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self.pose_sub_cb)
+		self.sub_state = rospy.Subscriber("/mavros/state", State, self.state_cb)
+		self.current_marker = None
+		self.velocity_setpoint = TwistStamped()
+	
+	def ar_pose_cb(self, msg):
+		markers =  msg.markers
+		if len(markers) = 0:
+			self.current_marker = None
+		else:
+			for marker in markers:
+				if marker.id in DISTANCES:
+					rospy.loginfo("Valid AR marker received")
+			min_dist = markers[0].pose.pose.position.z
+			self.current_marker = markers[0]
+			for marker in markers:
+				current_dist = marker.pose.pose.position.z
+				if current_dist < min_dist and marker.id in DISTANCES:
+					self.current_marker = marker
+					min_dist = current_dist
+		if self.current_marker is not None and self.current_marker.id in DISTANCES.keys():
+			ar_tag_pos = self.current_marker.pose.pose.position
+			i = self.current_marker.id
+				
+			err = Vector3(ar_tag_pos.z-DISTANCES[i][0],ar_tag_pos.x-DISTANCES[i][1],ar_tag_pos.y-DISTANCES[i][2])
+			rospy.loginfo(err)
+			self.pub_error_ar_tag.publish(err)
+			self.vel_ctrl()				
+
+	def line_vel_cb(self, vel_params):
+		self.velocity_setpoint.twist.linear.x = vel_params.x
+		self.velocity_setpoint.twist.linear.y = vel_params.y
+		self.velocity_setpoint.twist.angular.z = -vel_params.z
+
+	def set_vel_cmd(self):
+		if self.state == 1:
+			rospy.loginfo("Following line; looking for AR tag")
+			while self.current_marker is None:
+				pass
+			#self.state +=1
+			#Line follow until correct AR tag is detected within 5 meters
+		if self.state == 2:
+			while self.current_marker is not None:
+				x_err = abs(abs(self.current_marker.pose.pose.position.z)-AR_FWD_THRESH)
+				if x_err < AR_FWD_TOL:
+					self.state +=1
+			# P control the body up X, until within target range of AR tag
+			# Minimal, biased input against line detection
+		if self.state == 3:
+			self.velocity_setpoint.twist.linear.z = Z_LIN_SPEED
+			bd_z_err = DISTANCES[self.current_marker.id][2]-self.current_marker.pose.pose.position.y
+			duration = (bd_z_err)/Z_LIN_SPEED
+			timedelta = datetime.timedelta(seconds = duration)
+			
+			start_time = datetime.datetime.now()
+			while not rospy.is_shutdown() and datetime.now() - start_time < timedelta:
+			    rospy.loginfo("flying_up")
+		    	self.rate.sleep()
+		    self.state+=1
+			#Complete open loop control, fly up/down using dead reckoning
+		if self.state == 4:
+			#OLC with fly forward
+		if self.state == 5
+			# OLC with fly opposite of state 3
+			# Should rever bak to state 1
+
+	def start_streaming_offboard_points(self):
+		""" Starts thread that will publish yawrate at `rate` in Hz
+		"""
+		def run_streaming():
+			self.offboard_point_streaming = True
+			rospy.loginfo("Streaming")
+			self.set_vel_cmd()
+			while ((not rospy.is_shutdown()) and self.offboard_point_streaming) or NO_ROBOT:
+				
+				if (self.velocity_setpoint is not None):
+					# limit speed for safety
+					velocity_setpoint_limited = deepcopy(self.velocity_setpoint)
+					speed = np.linalg.norm([velocity_setpoint_limited.twist.linear.x,
+											velocity_setpoint_limited.twist.linear.y,
+											velocity_setpoint_limited.twist.linear.z])
+					if speed > MAX_LIN_SPEED:
+						velocity_setpoint_limited.twist.linear.x *= MAX_LIN_SPEED / speed
+						velocity_setpoint_limited.twist.linear.y *= MAX_LIN_SPEED / speed
+						velocity_setpoint_limited.twist.linear.z *= MAX_LIN_SPEED / speed
+					yaw_cmd = velocity_setpoint_limited.twist.angular.z 
+					if np.absolute(yaw_cmd) > MAX_ANG_SPEED:
+						yaw_cmd = np.sign(yaw_cmd) * MAX_ANG_SPEED
+					velocity_setpoint_limited.twist.angular.z  = yaw_cmd
+					# Publish limited setpoint
+					self.pub_local_velocity_setpoint.publish(velocity_setpoint_limited)
+				self.rate.sleep()
+				#self.velocity_setpoint = TwistStamped()
+				#self.pub_local_velocity_setpoint.publish(self.velocity_setpoint)
+				#set default, no line detected behavior here
+				
+
+		self.offboard_point_streaming_thread = threading.Thread(target=run_streaming)
+		self.offboard_point_streaming_thread.start()
+
+	def stop_streaming_offboard_points(self):
+		""" Safely terminates offboard publisher
+		"""
+		self.offboard_point_streaming = False
+		try:
+			self.offboard_point_streaming_thread.join()
+		except AttributeError:
+			pass
+	def state_cb(self, state):
+		""" Starts setpoint streamer when mode is "POSCTL" and disables it when mode is "MANUAL"
+		:param state: Given by subscribed topic `/mavros/state`
+		"""
+		self.current_state = state
+		mode = getattr(state, "mode", None)
+		if (mode == "POSCTL" or NO_ROBOT) and not self.offboard_point_streaming:
+			rospy.loginfo("Setpoint stream ENABLED")
+			self.start_streaming_offboard_points()
+		elif mode == "MANUAL" and self.offboard_point_streaming:
+			rospy.loginfo("Setpoint stream DISABLED")
+			self.stop_streaming_offboard_points()
+
+
+if __name__ == "__main__":
+	rospy.init_node("final_line_ar_obstacle")
+	ctrl = FinalChallengeController()
+	rospy.loginfo("Starting final challenge controller")
+	rospy.spin()
+	ctrl.stop_streaming_offboard_points()
---
+++ final_challenge/src/line_tracker.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+import rospy
+import threading
+import numpy as np
+from datetime import datetime
+from geometry_msgs.msg import TwistStamped, Twist, PoseStamped, Quaternion, Point, Vector3
+from sensor_msgs.msg import Image
+from aero_control.msg import Line
+from std_msgs.msg import Float32
+from copy import deepcopy
+
+K_P_X = 0.04 # TODO: decide upon initial K_P_X
+K_P_Y = 0.02 # TODO: decide upon initial K_P_Y
+K_P_Z = 0.02 # TODO: decide upon initial K_P_Z
+K_D_Y = 0.012
+K_I_Y = 0.0
+K_P_ANG_Z = 1.5
+K_D_ANG_Z = 0.0
+K_I_ANG_Z = 0.0
+CENTER = (64, 64)
+DIST = 50
+TIMEOUT_PERIOD = 1.0
+
+#Responsible of sending velocity commands for line tracking
+
+class LineTracker:
+
+	def __init__(self, rate=10):
+		""" Initializes publishers and subscribers, sets initial values for vars
+		:param rate: the rate at which the setpoint_velocity is published
+		"""
+		assert rate > 2 # make sure rate is high enough, if new setpoint recieved within .5 seconds, robot will switch back to POSCTL
+		self.rate_hz = rate        
+		self.rate = rospy.Rate(rate)
+		
+		
+		self.pub_pid_vel = rospy.Publisher("/line_detection/cmd_vel", Vector3, queue_size=1)
+		self.sub_line_param = rospy.Subscriber("/line_detection/line", Line, self.line_param_cb)
+		
+		# Variables dealing with publishing setpoint
+		
+		self.prev_y_err = 0.0
+		self.sum_y_err = 0.0
+		self.prev_ang_err = 0.0
+		self.sum_ang_err = 0.0
+		
+		self.velocity_setpoint = TwistStamped()
+		self.t_line_last_seen = None
+		while True:
+			if self.t_line_last_seen is None:
+				self.pub_pid_vel.publish(Vector3(0,0,0))
+			else:
+				self.td = datetime.now() - self.t_line_last_seen
+				if self.td.total_seconds()>TIMEOUT_PERIOD:
+					rospy.loginfo("Line lost")
+					self.pub_pid_vel.publish(Vector3(0,0,0))
+					self.t_line_last_seen = datetime.now()
+
+
+	@staticmethod
+	def conv_vect(v_x, v_y):
+		if v_x<0:
+		  v_x = -1*v_x
+		  v_y = -1*v_y
+		return(v_x, v_y)
+
+	@staticmethod
+	def find_closest_point(x, y, v_x, v_y):
+		m_reg = v_y/v_x
+		b_reg = y - m_reg * x
+		if m_reg == 0:
+			m_reg = 0.0001
+		b_perp = CENTER[1]+CENTER[0]/m_reg
+		#This is the point of intersection between the line perpendicular to the received line
+		#containing the drone's position and the received line
+		xc = ((m_reg*(b_perp-b_reg)) / (m_reg**2 +1))
+		yc =-xc/m_reg + b_perp 
+		return xc, yc
+
+	@staticmethod
+	def find_error(xc, yc):
+		return (xc-CENTER[0], yc-CENTER[1])
+
+	@staticmethod
+	def d_target_position(xc, yc, v_x, v_y):
+		#Gives distance from drone current position to target point
+		#Target point is DIST away from the closest point, in the +x direction
+		theta=np.arctan(v_y/v_x)
+		x_f = xc + DIST*np.cos(theta)
+		y_f = yc + DIST*np.sin(theta)
+		return(x_f - CENTER[0], y_f - CENTER[1])
+
+	def pid_control(self, pos, ang_err):
+		vel_cmd_x =  K_P_X * pos[0]
+		dt = 1.0/self.rate_hz
+		vel_cmd_y =  (K_P_Y * pos[1]+ K_D_Y * (pos[1]-self.prev_y_err)/dt  + K_I_Y * self.sum_y_err) #Set negative due to BU frame of reference compared to downward camera
+		yaw_cmd = - (K_P_ANG_Z * ang_err + K_D_ANG_Z * (ang_err-self.prev_ang_err)/dt + K_I_ANG_Z * self.sum_ang_err)
+		return (vel_cmd_x,vel_cmd_y, yaw_cmd)
+	
+	def line_param_cb(self, line_params):
+		vx,vy = self.conv_vect(line_params.vx, line_params.vy)
+		if vx ==0:
+			vx = 0.001
+		xc,yc =  self.find_closest_point(line_params.x, line_params.y, vx, vy)
+		
+		self.ang_err = np.arctan(vy/vx)
+
+		self.pos = self.d_target_position(xc, yc, vx, vy)
+
+		cmd_x, cmd_y, cmd_yaw = self.pid_control(self.pos, self.ang_err)
+
+		self.prev_ang_err = self.ang_err
+		self.prev_y_err = self.pos[1]
+		self.sum_ang_err+=self.ang_err
+		self.sum_y_err+=self.pos[1]
+		self.pub_pid_vel.publish(Vector3(cmd_x,cmd_y,cmd_yaw))
+		self.t_line_last_seen = datetime.now()
+		
+
+if __name__ == "__main__":
+	rospy.init_node("line_tracker")
+	d = LineTracker()
+	rospy.spin()
